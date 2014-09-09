ac2c69d5d10972064eea019bcccf6a43f2c35a93
Ashley <a.clarkem@gmail.com>
2014-09-09 12:45:09 -0600

    user cannot add the same category to a kitten twice

--- app/controllers/kittens_categories_controller.rb
+++ app/controllers/kittens_categories_controller.rb
@@ -5,16 +5,21 @@ class KittensCategoriesController < ApplicationController
   end
 
   def create
+    @kittens_categories = KittensCategory.new
     @categories = Category.all
-    @kittens_categories = KittensCategory.create(kitten_id: params[:id], category_id: params[:kittens_category][:category_id])
-    if @kittens_categories.save
-      redirect_to root_path
+
+    if KittensCategory.find_by(kitten_id: params[:id], category_id: params[:kittens_category][:category_id]) == nil
+      @kittens_categories = KittensCategory.create(kitten_id: params[:id], category_id: params[:kittens_category][:category_id])
+      if @kittens_categories.save
+        redirect_to root_path
+      else
+        flash[:notice] = "CATegory cannot be blank"
+        render 'new'
+      end
     else
+      flash[:notice] = "CAtegory has already been added"
       render 'new'
     end
   end
 
-  # def kitcat_params
-  # params.require(:kittens_category).permit(:category_id, :id)
-  # end
 end
