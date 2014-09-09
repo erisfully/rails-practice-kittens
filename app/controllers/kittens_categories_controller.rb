817684198df8f23808e933cb86c702a2f5d4d1ad
Ashley <a.clarkem@gmail.com>
2014-09-09 12:54:37 -0600

    flash message displays properly

--- app/controllers/kittens_categories_controller.rb
+++ app/controllers/kittens_categories_controller.rb
@@ -8,17 +8,17 @@ class KittensCategoriesController < ApplicationController
     @kittens_categories = KittensCategory.new
     @categories = Category.all
 
-    if KittensCategory.find_by(kitten_id: params[:id], category_id: params[:kittens_category][:category_id]) == nil
+    if KittensCategory.find_by(kitten_id: params[:id], category_id: params[:kittens_category][:category_id]) != nil
+      flash.now[:notice] = "CAtegory has already been added"
+      render 'new'
+    else
       @kittens_categories = KittensCategory.create(kitten_id: params[:id], category_id: params[:kittens_category][:category_id])
       if @kittens_categories.save
         redirect_to root_path
       else
-        flash[:notice] = "CATegory cannot be blank"
+        flash.now[:notice] = "CATegory cannot be blank"
         render 'new'
       end
-    else
-      flash[:notice] = "CAtegory has already been added"
-      render 'new'
     end
   end
 
